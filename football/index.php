03448201b1307a0d26c2d862db079479cd39cbee
Brian1011 <mutindabrian26@gmail.com>
2018-10-30 12:44:44 +0300

    Add boot strap to homepage

--- football/index.php
+++ football/index.php
@@ -11,7 +11,7 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 
 		<!-- Latest compiled JavaScript -->
-		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
 		<link rel="stylesheet" type="text/css" href="css/custom_css.css">
 
